1512dde45ce539407f56d98dc29960ce7ecae4b8
crakkbot <christian.halmer@gmail.com>
2018-05-01 22:48:20 +0200

    added variable z

--- src/lol.java
+++ src/lol.java
@@ -1,7 +1,10 @@
+import java.math.BigInteger;
+
 public class lol {
     public static void main(String[] args) {
         int x = 5;
-        int y = 4;
-        int z = 3;
+        int y = 1;
+        int z = 2147483647;
+        System.out.println(z+y);
     }
 }
